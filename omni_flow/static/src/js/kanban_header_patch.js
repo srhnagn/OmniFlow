f1e5221db5edd150618978154eaf23b58c03a248
srhnagn <serhanagan68@gmail.com>
2026-08-17 09:41:29 +0300

    fix: To-do folding memory, Waffle menu z-index, and cog visibility rules

--- omni_flow/static/src/js/kanban_header_patch.js
+++ omni_flow/static/src/js/kanban_header_patch.js
@@ -9,10 +9,10 @@ patch(Group.prototype, {
         super.setup(...arguments);
         const resModel = this.resModel || (this.config && this.config.resModel) || (this.model && this.model.resModel);
         
-        if (resModel === "project.task" && this.groupByField && this.groupByField.name === "omni_state" && this.value) {
+        if (resModel === "project.task" && this.groupByField && (this.groupByField.name === "omni_state" || this.groupByField.name === "personal_stage_type_id") && this.value) {
             const stateVal = Array.isArray(this.value) ? this.value[0] : this.value;
             if (stateVal) {
-                const storageKey = `omniflow_fold_${stateVal}`;
+                const storageKey = `omniflow_fold_${this.groupByField.name}_${stateVal}`;
                 const savedState = browser.localStorage.getItem(storageKey);
                 if (savedState === "true") {
                     this.config.isFolded = true;
@@ -29,12 +29,12 @@ patch(Group.prototype, {
         // Native toggle
         const result = await super.toggle(...arguments);
         
-        // Handle localStorage persistence for omni_state
-        if (resModel === "project.task" && this.groupByField && this.groupByField.name === "omni_state" && this.value) {
+        // Handle localStorage persistence for omni_state and personal_stage_type_id
+        if (resModel === "project.task" && this.groupByField && (this.groupByField.name === "omni_state" || this.groupByField.name === "personal_stage_type_id") && this.value) {
             const stateVal = Array.isArray(this.value) ? this.value[0] : this.value;
             if (stateVal) {
                 const isFolded = this.config ? this.config.isFolded : this.isFolded;
-                const storageKey = `omniflow_fold_${stateVal}`;
+                const storageKey = `omniflow_fold_${this.groupByField.name}_${stateVal}`;
                 browser.localStorage.setItem(storageKey, isFolded ? "true" : "false");
             }
         }
